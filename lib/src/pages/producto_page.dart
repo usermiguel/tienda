2bf14723dafc1f02c39b137f58ac1cc06f8037c2
Miguel <i1720539@continental.edu.pe>
2021-03-10 00:01:16 -0500

    ProductModel

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/utils/utils.dart' as utils;
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +29,9 @@ class ProductPage extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(15.0),
           child: Form(
+            key: formKey,
             child: Column(
-              children: [
+              children: <Widget>[
                 _crearNombre(),
                 _crearPrecio(),
                 _crearBoton(),
@@ -40,15 +49,29 @@ class ProductPage extends StatelessWidget {
       decoration: InputDecoration(
         labelText: 'Producto',
       ),
+      validator: (value) {
+        if (value.length < 3) {
+          return 'Ingrese el nombre del producto';
+        } else {
+          return null;
+        }
+      },
     );
   }
 
   Widget _crearPrecio() {
     return TextFormField(
-      keyboardType: TextInputType.number,
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
       decoration: InputDecoration(
         labelText: 'Precio',
       ),
+      validator: (value) {
+        if (utils.isNumeric(value)) {
+          return null;
+        } else {
+          return 'Solo números';
+        }
+      },
     );
   }
 
@@ -61,7 +84,12 @@ class ProductPage extends StatelessWidget {
       label: Text('Guardar'),
       textColor: Colors.white,
       icon: Icon(Icons.save),
-      onPressed: () {},
+      onPressed: _submit,
     );
+  }
+
+  void _submit() {
+    if (!formKey.currentState.validate()) return;
+    print('todo ok');
   }
 }
